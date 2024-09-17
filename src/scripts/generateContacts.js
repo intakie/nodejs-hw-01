b31310243acb9dbbc90d9036340c4ffc6594dbbe
intakie <intakiea@gmail.com>
2024-09-17 16:52:44 +0300

    upd (1)

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+
+  for (let i = 0; i < number; i++) {
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+  }
+
+  await writeContacts(contacts);
+  console.log(`${number} contacts added successfully.`);
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,11 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  contacts.length = contacts.length - 1;
+
+  await writeContacts(contacts);
+};
 
 removeLastContact();
